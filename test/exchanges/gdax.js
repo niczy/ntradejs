f9a89f90e367bb8156a89c463aa2435c867398c9
Yu Zhao <nicholas.zhaoyu@gmail.com>
2017-09-07 20:38:19 -0700

    Add test for gdax#getPortfolio

--- test/exchanges/gdax.js
+++ test/exchanges/gdax.js
@@ -7,29 +7,30 @@ var dirs = util.dirs();
 var config = util.getConfig();
 
 var TRADES = require('./data/gdax_trades.json');
+var PORTFOLIO = require('./data/gdax_portfolio.json');
 var FakePublicClient = function() {};
 FakePublicClient.prototype = {
- getProductTrades: function(option, callback){
+  getProductTrades : function(option, callback) {
     callback(null, null, TRADES);
-  }
+  },
 };
 
 var FakeAuthenticatedClient = function() {};
 FakeAuthenticatedClient.prototype = {
+  getAccounts : function(callback) { callback(null, null, PORTFOLIO); },
 };
 
 var FakeExchange = {
-  PublicClient: FakePublicClient,
-  AuthenticatedClient: FakeAuthenticatedClient,
-  '@noCallThru': true
+  PublicClient : FakePublicClient,
+  AuthenticatedClient : FakeAuthenticatedClient,
+  '@noCallThru' : true
 };
 
-var stubs = {
-  'gdax': FakeExchange
-};
+var stubs = {'gdax' : FakeExchange};
 
 describe('exchange/gdax', function() {
   const Gdax = proxyquire(dirs.exchanges + 'gdax', stubs);
+  // const Gdax = require(dirs.exchanges + 'gdax');
   const gdax = new Gdax(config);
 
   it('should correctly parse historical trades', function(done) {
@@ -39,5 +40,18 @@ describe('exchange/gdax', function() {
       done();
     };
     gdax.getTrades(null, check, false);
+  });
+
+  it('should correctly parse portfolio data', function(done) {
+    var check = function(err, portfolio) {
+      expect(err).to.equal(null);
+      const expectedPortfolio = [
+        {name : 'USD', amount : 0}, {name : 'LTC', amount : 0},
+        {name : 'ETH', amount : 0.018}, {name : 'BTC', amount : 0.0140153}
+      ];
+      expect(portfolio).to.deep.equal(expectedPortfolio);
+      done();
+    };
+    gdax.getPortfolio(check);
   });
 });
