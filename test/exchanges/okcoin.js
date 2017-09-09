3525b42703f121ad626e92f5898d404874a005db
Yu Zhao <nicholas.zhaoyu@gmail.com>
2017-09-08 17:19:15 -0700

    Add test for get okcoin#getticker

--- test/exchanges/okcoin.js
+++ test/exchanges/okcoin.js
@@ -7,9 +7,11 @@ var dirs = util.dirs();
 var config = util.getConfig();
 
 var PORTFOLIO = require('./data/okcoin_portfolio.json');
+var TICKER = require('./data/okcoiin_ticker.json');
 var OkCoin = function() {};
 OkCoin.prototype = {
-  getUserInfo : function(callback) { callback(null, PORTFOLIO); }
+  getUserInfo : function(callback) { callback(null, PORTFOLIO); },
+  getTicker: function(callback) { callback(null, TICKER); }
 };
 
 var stubs = {'okcoin-china' : OkCoin};
@@ -29,5 +31,15 @@ describe('exchange/okcoin', function() {
       done();
     };
     okcoin.getPortfolio(check);
+  });
+
+  it('Should correct parse ticker data', function(done) {
+    var check = function(err, ticker) {
+      expect(err).to.equal(null);
+      const expectedTicker = {"buy":"24017.99","high":"29007.99","last":"24017.99","low":"22888.55","sell":"24020.0","vol":"40108.0735003","bid":24020,"ask":24017.99};
+      expect(ticker).to.deep.equal(expectedTicker);
+      done();
+    };
+    okcoin.getTicker(check);
   });
 });
